3e6ddd9c6492a076b1efc3940da001fb8678f0fe
samanthabertous <sam@samanthabertous.com>
2016-04-29 21:24:41 -0400

    Created wordpress theme based on a previously static theme

---
+++ wp-content/themes/secretlyshameless_portfolio/sidebar-page.php
@@ -0,0 +1,8 @@
+  <!-- Secondary Column -->
+<div class="small-12 medium-4 medium-pull-8 columns">
+	<div class="secondary">
+		<?php if (!dynamic_sidebar('page')) : ?>
+
+		<?php endif; ?>
+	</div>
+</div>
